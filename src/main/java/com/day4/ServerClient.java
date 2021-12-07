d91004756ef7dec84cea483a34b7faca5db0ffe7
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-07 09:36:40 +0800

    creates maven file structure

---
+++ src/main/java/com/day4/ServerClient.java
@@ -0,0 +1,9 @@
+package com.day4;
+
+public class ServerClient 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/day4/ServerClientTest.java
@@ -0,0 +1,15 @@
+package com.day4;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ServerClientTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
