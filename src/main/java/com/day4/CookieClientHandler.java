9db9edec20220bbac54465bc13bd7c89b2e4badf
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-12 01:37:32 +0800

    rewrite CookiesClientHandler to handle thread

--- src/main/java/com/day4/CookieClientHandler.java
+++ src/main/java/com/day4/CookieClientHandler.java
@@ -2,56 +2,98 @@ package com.day4;
 
 import java.io.*;
 import java.net.*;
+import java.util.List;
+import java.util.Random;
 
 class CookieClientHandler implements Runnable
 {
-    private final Socket clientSocket;
+    private Socket socket;
+    private List<String> listofCookies;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
 
-    public CookieClientHandler(Socket socket)
+    public CookieClientHandler(Socket socket, String filename)
     {
-        this.clientSocket = socket;
+        try 
+        {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Cookie cookie = new Cookie(filename);
+            listofCookies = cookie.readCookie();
+        } 
+        catch (IOException e) 
+        {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+            e.printStackTrace();
+        }
     }
 
-    public void run()
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter)
     {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        try {
-                
-              // get the outputstream of client
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-              // get the inputstream of client
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            String line;
-            while ((line = in.readLine()) != null) 
-            {
-                // writing the received message from client
-                System.out.printf("Client: %s\n",line);
-                out.println(line);
-            }
+        try
+        {
+            if (bufferedReader!=null) bufferedReader.close();
+            if (bufferedWriter!=null) bufferedWriter.close();
+            if (socket!=null) socket.close();
         }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    // function to send msg from server to client
+    public void sendmsg(BufferedWriter bufferedWriter, String msg)
+    {
+        try 
+        {
+            bufferedWriter.write(msg);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } 
         catch (IOException e) 
         {
             e.printStackTrace();
         }
-        finally
+    }
+
+    @Override
+    public void run()
+    {
+        while (socket.isConnected())
         {
-            try {
-                if (out != null)
-                {
-                    out.close();
-                } 
-                if (in != null) 
-                {
-                    in.close();
-                    clientSocket.close();
-                }
-            }
-            catch (IOException e) 
+            try
             {
+                while (true)
+                {
+                    String msgfromclient = bufferedReader.readLine(); //receive msg from client
+                    System.out.println("Client: " + msgfromclient);
+                    
+                    if (msgfromclient.equalsIgnoreCase("close"))
+                    {
+                        bufferedReader.close();
+                        bufferedWriter.close();
+                        socket.close();
+                        break;
+
+                    }
+                    else if (msgfromclient.equalsIgnoreCase("get-cookies"))
+                    {
+                        String randomCookie = listofCookies.get(new Random().nextInt(listofCookies.size()));
+                        sendmsg(bufferedWriter, "cookie-text " + randomCookie);
+                    }
+                    else
+                    {
+                        sendmsg(bufferedWriter, "Wrong command. Only 2 commands(get-cookies and close) are accepted.");
+                    }
+                }
+            } 
+            catch (IOException e)
+            {
+                closeEverything(socket, bufferedReader, bufferedWriter);
                 e.printStackTrace();
+                break;
             }
         }
     }
